0c605be177ad9684047b36a0b788a96409a4435e
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-02-02 17:16:12 +0530

    [Chandra]-"CoffeMaker"

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MSTtestDemo_Coffeemakerdemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
